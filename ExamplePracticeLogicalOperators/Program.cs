7a684b88e8e8705ba2044ca0c40d37e774c2a1ad
kaveeja1999 <87494020+IT21117428@users.noreply.github.com>
2024-10-22 12:49:40 +0530

    Logical operators exercise

---
+++ ExamplePracticeLogicalOperators/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// First Example
+
+int age = 20;
+bool hasLicense = true;
+bool consumedAlcohol = false;
+
+bool canDrive = age > 18 && hasLicense && !consumedAlcohol;
+
+Console.WriteLine(canDrive);
+
+// Second Example
+
+bool isCardValid = true;
+bool isCardExpired = false;
+double transactionAmount = 150;
+double transactionLimit = 2000;
+
+bool isPaymentSuccessful = isCardValid && !isCardExpired && (transactionAmount <= transactionLimit);
+
+Console.WriteLine(isPaymentSuccessful);
+
+
+// Third Example
+
+bool hasFreeTimeToPlay = false;
+bool hasOtherPets = true;
+bool hasBackyard = true;
+bool hasPetAlegies = false;
+
+bool canAdoptPet = (!hasFreeTimeToPlay || hasOtherPets) && hasBackyard && !hasPetAlegies;
+    
+Console.WriteLine(canAdoptPet);
