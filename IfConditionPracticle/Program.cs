b1a5b2021e08498f70a04540be9f4e25386619ba
kaveeja1999 <87494020+IT21117428@users.noreply.github.com>
2024-10-25 01:28:37 +0530

    If condition example practice

---
+++ IfConditionPracticle/Program.cs
@@ -0,0 +1,114 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Exercise 1: Even or Odd
+
+//Console.WriteLine("Enter the number: ");
+//int number = int.Parse(Console.ReadLine());
+
+//if (number % 2  == 0)
+//{
+//    Console.WriteLine("This is even number");
+//}
+//else
+//{
+//    Console.WriteLine("This is odd number");
+//}
+
+//Exercise 2: Grade Checker
+
+//Console.WriteLine("Enter the mark of student : ");
+//double marks = double.Parse(Console.ReadLine());
+//Math.Round(marks, 1);
+
+//if (marks > 90)
+//{
+//    Console.WriteLine("A grade");
+//}
+//else if (marks > 80)
+//{
+//    Console.WriteLine("B Grade");
+//}
+//else if (marks > 70)
+//{
+//    Console.WriteLine("C Grade");
+//}
+//else if (marks > 60)
+//{
+//    Console.WriteLine("D Grade");
+//}
+//else
+//{
+//    Console.WriteLine("F Grade");
+//}
+
+//Exercise 3: Age Verification
+
+//Console.WriteLine("Enter your age: ");
+//int age = int.Parse(Console.ReadLine());
+
+//if (age >= 18 && age < 65)
+//{
+//    Console.WriteLine("You are eligible to vote");
+//}
+//else if (age > 65)
+//{
+//    Console.WriteLine("You are eligible to vote. You are also a senior citizen.");
+//}
+//else
+//{
+//    Console.WriteLine("You are not eligible to cote becouse you are teenager");
+//}
+
+//Exercise 4: Smallest Number Finder
+
+//Console.WriteLine("Enter the first number : ");
+//int num1 = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter the second number : ");
+//int num2 = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter the third number : ");
+//int num3 = int.Parse(Console.ReadLine());
+
+//int smallest;
+
+
+//if (num1 <= num2)
+//{
+//    if (num1 <= num3)
+//    {
+//        smallest = num1;
+//    }
+//    else
+//    {
+//        smallest = num3;
+//    }
+//}
+//else if (num2 <= num3) { 
+
+//    smallest = num2;
+
+//}
+//else
+//{
+//    smallest = num3;
+//}
+
+//Console.WriteLine("The smallest number is : " + smallest);
+
+//Exercise 5: Login System
+
+//Console.WriteLine("Enter the User name : ");
+//string userName = Console.ReadLine();
+
+//Console.WriteLine("Enter the password : ");
+//string passWord = Console.ReadLine();
+
+//if (userName == "user123" &&  passWord == "pass123")
+//{
+//    Console.WriteLine("Login successful");
+//}
+//else
+//{
+//    Console.WriteLine("Login faile, please cheack your user name and password");
+//}
